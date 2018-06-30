6c51711be0c8c31ff28229a46d259aab5f580d7b
Jake Klassen <jklassendev@gmail.com>
2018-06-29 22:47:45 -0400

    package upgrades and formatting changes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 /* eslint-disable no-unused-vars */
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({
     error: err.message,
--- test/routes/index.js
+++ test/routes/index.js
@@ -5,21 +5,27 @@ import app from '../../src';
 test.cb('index route', t => {
   t.plan(2);
 
-  request(app).get('/').expect(200).end((err, res) => {
-    t.falsy(err);
-    t.is(res.body.message, 'o hai!');
+  request(app)
+    .get('/')
+    .expect(200)
+    .end((err, res) => {
+      t.falsy(err);
+      t.is(res.body.message, 'o hai!');
 
-    t.end();
-  });
+      t.end();
+    });
 });
 
 test.cb('404', t => {
   t.plan(2);
 
-  request(app).get('/nope').expect(404).end((err, res) => {
-    t.falsy(err);
-    t.is(res.body.error, '/nope - 404 Not Found');
+  request(app)
+    .get('/nope')
+    .expect(404)
+    .end((err, res) => {
+      t.falsy(err);
+      t.is(res.body.error, '/nope - 404 Not Found');
 
-    t.end();
-  });
+      t.end();
+    });
 });
